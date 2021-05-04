8cbc570488ce664aed97d9829ea1bcef654bb870
DEEPALI-SUK <sukhijadeepali2001@gmail.com>
2021-05-04 12:01:52 +0530

    login register admin dashboard added

---
+++ static/js/admindashboard.js
@@ -0,0 +1,19 @@
+// Sidebar Toggle Codes;
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+var sidebarCloseIcon = document.getElementById("sidebarIcon");
+
+function toggleSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add("sidebar_responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove("sidebar_responsive");
+    sidebarOpen = false;
+  }
+}
